5f19afec17f75b27fa680d49705bafbb1a8163cc
sergeyJavaDev <76526505+sergeyJavaDev@users.noreply.github.com>
2021-10-12 13:13:12 +0500

    Update ErrorController.java

--- src/main/java/net/servlet/page/ErrorController.java
+++ src/main/java/net/servlet/page/ErrorController.java
@@ -10,11 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import net.servlet.AbstractController;
 import net.util.RoutingUtils;
 
-/**
- * 
- * @author devstudy
- * @see http://devstudy.net
- */
+
 @WebServlet("/error")
 public class ErrorController extends AbstractController {
 	private static final long serialVersionUID = -4385792519039493271L;
